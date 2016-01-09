457fd1948433183ffc79a702808aef346a6e1611
Alex Rousskov <rousskov@measurement-factory.com>
2016-01-09 02:10:06 -0700

    Simplified: Reduce nesting, avoid Mocha, and use recently added features.
    Mocha is nice for dealing with a large number of small, fast,
    isolated test cases [accumulated in one file]. However, when dealing
    with a relatively long-running test that has multiple sub-cases, the
    describe/it() architecture and inability to specify command line
    parameters starts to get in the way.
    
    I suspect this is especially true if one tries to run multiple such
    tests in parallel, although that might be best done using parallel(1)
    or similar external tools and not Javascript code (unless some
    coordination among parallel tasks is needed).

--- tests/test-proxy-must-update-headers-after-304.js
+++ tests/test-proxy-must-update-headers-after-304.js
@@ -1,11 +1,11 @@
 // Proxy MUST update previously cached headers on 304 responses.
 //
-// mocha --bail --require tests/mocha-config tests/test-...
+// babel-node --optional es7.asyncFunctions tests/test-...
 
 import Promise from "bluebird";
 import ProxyCase from "./ProxyCase";
-import * as Config from "../src/misc/Config";
 import * as Uri from "../src/anyp/Uri";
+import Body from "../src/http/Body";
 import Resource from "../src/anyp/Resource";
 import * as FuzzyTime from "../src/misc/FuzzyTime";
 import assert from "assert";
@@ -16,77 +16,73 @@ process.on("unhandledRejection", function (reason /*, promise */) {
 });
 Promise.config({ warnings: true });
 
-Config.DefaultMessageBodyContent = Array(2*32*1024).join("x");
-
-const Hour = new Date(60*60*1000); // 1 hour delta
-
 // TODO: Optionally tolerate any misses (mostly useful for parallel/life tests).
 
-let resource = new Resource();
-resource.uri = Uri.Unique();
-resource.modifiedAt(FuzzyTime.DistantPast());
-resource.expireAt(FuzzyTime.Soon());
+async function Test() {
 
-let UpdatingResponse = null; // TBD
+    let resource = new Resource();
+    resource.uri = Uri.Unique();
+    resource.modifiedAt(FuzzyTime.DistantPast());
+    resource.expireAt(FuzzyTime.Soon());
+    resource.body = new Body("x".repeat(64*1024));
 
-let steps = [
-
-    () => {
+    {
         let testCase = new ProxyCase('forward a cachable response');
         testCase.client().request.for(resource);
         testCase.server().serve(resource);
         testCase.server().response.tag("first");
-        return testCase;
-    },
+        await testCase.run();
+    }
 
-    () => {
+    {
         let testCase = new ProxyCase('respond with a 304 hit');
         testCase.client().request.for(resource);
-        testCase.client().request.conditions({ ims: resource.notModifiedSince(Hour) });
+        testCase.client().request.conditions({ ims: resource.notModifiedSince() });
         testCase.check(() => {
             testCase.expectStatusCode(304);
         });
-        return testCase;
-    },
+        await testCase.run();
+    }
 
-    () => {
+    let updatingResponse = null; // TBD
+    {
         let testCase = new ProxyCase('miss and get a 304 that updates the previously cached response');
 
         resource.modifyNow();
         resource.expireAt(FuzzyTime.DistantFuture());
+
         testCase.client().request.for(resource);
-        testCase.client().request.conditions({ ims: resource.modifiedSince(Hour) });
+        testCase.client().request.conditions({ ims: resource.modifiedSince() });
         testCase.client().request.header.add("Cache-Control", "max-age=0");
+
         testCase.server().serve(resource);
         testCase.server().response.tag("second");
         testCase.server().response.startLine.statusCode = 304;
+
         testCase.check(() => {
             testCase.expectStatusCode(200);
             // XXX: Check the headers.
-            UpdatingResponse = testCase.server().transaction().response;
+            updatingResponse = testCase.server().transaction().response;
         });
 
-        return testCase;
-    },
+        await testCase.run();
+    }
 
-    () => {
+    {
         let testCase = new ProxyCase('hit updated headers');
         testCase.client().request.for(resource);
         testCase.check(() => {
             testCase.expectStatusCode(200);
             let updatedResponse = testCase.client().transaction().response;
-            assert.equal(updatedResponse.tag(), UpdatingResponse.tag(), "updated X-Daft-Response-Tag");
-            assert.equal(updatedResponse.id(), UpdatingResponse.id(), "updated X-Daft-Response-ID");
+            assert.equal(updatedResponse.tag(), updatingResponse.tag(), "updated X-Daft-Response-Tag");
+            assert.equal(updatedResponse.id(), updatingResponse.id(), "updated X-Daft-Response-ID");
             assert.equal(updatedResponse.header.values("Last-Modified"), resource.lastModificationTime.toUTCString(), "updated Last-Modified");
             assert.equal(updatedResponse.header.values("Expires"), resource.nextModificationTime.toUTCString(), "updated Expires");
         });
-        return testCase;
+        await testCase.run();
     }
 
-];
+    console.log("Test result: success");
+}
 
-describe('proxy', function () {
-    it('MUST update previously cached headers on 304 responses', async function () {
-        await Promise.each(steps, step => step().run());
-    });
-});
+Test();
