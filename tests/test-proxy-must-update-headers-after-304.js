f246260492e9912a7e371dccd515abda865baceb
Alex Rousskov <rousskov@measurement-factory.com>
2016-01-16 22:31:39 -0700

    Added project boilerplate to most sources.

--- tests/test-proxy-must-update-headers-after-304.js
+++ tests/test-proxy-must-update-headers-after-304.js
@@ -1,3 +1,7 @@
+/* Daft Toolkit                         http://www.measurement-factory.com/
+ * Copyright (C) 2015,2016 The Measurement Factory.
+ * Licensed under the Apache License, Version 2.0.                       */
+
 // Proxy MUST update previously cached headers on 304 responses.
 //
 // babel-node --optional es7.asyncFunctions tests/test-...
