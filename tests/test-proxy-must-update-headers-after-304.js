b976dba61530a103898e9b054c0b6d89fef13b72
Alex Rousskov <rousskov@measurement-factory.com>
2019-03-24 21:43:52 -0600

    Fixed code formatting using UniForm

--- tests/test-proxy-must-update-headers-after-304.js
+++ tests/test-proxy-must-update-headers-after-304.js
@@ -21,87 +21,87 @@ export default class MyTest extends Test {
 
     async run(/*testRun*/) {
 
-    let resource = new Resource();
-    resource.uri.address = AddressPool.ReserveListeningAddress();
-    resource.modifiedAt(FuzzyTime.DistantPast());
-    resource.expireAt(FuzzyTime.Soon());
-    resource.body = new Body("x".repeat(64*1024));
-    resource.finalize();
+        let resource = new Resource();
+        resource.uri.address = AddressPool.ReserveListeningAddress();
+        resource.modifiedAt(FuzzyTime.DistantPast());
+        resource.expireAt(FuzzyTime.Soon());
+        resource.body = new Body("x".repeat(64*1024));
+        resource.finalize();
 
-    // This header appears in the initially cached response.
-    // This header does not appear in the updatingResponse.
-    // This header must upppear in the updatedResponse.
-    const hitCheck = new Field("X-Daft-Hit-Check", Gadgets.UniqueId("check"));
+        // This header appears in the initially cached response.
+        // This header does not appear in the updatingResponse.
+        // This header must upppear in the updatedResponse.
+        const hitCheck = new Field("X-Daft-Hit-Check", Gadgets.UniqueId("check"));
 
-    {
-        let testCase = new ProxyCase('forward a cachable response');
-        testCase.client().request.for(resource);
-        testCase.server().serve(resource);
-        testCase.server().response.tag("first");
-        testCase.server().response.header.add(hitCheck);
-        await testCase.run();
+        {
+            let testCase = new ProxyCase('forward a cachable response');
+            testCase.client().request.for(resource);
+            testCase.server().serve(resource);
+            testCase.server().response.tag("first");
+            testCase.server().response.header.add(hitCheck);
+            await testCase.run();
+        }
+
+        {
+            let testCase = new ProxyCase('respond with a 304 hit');
+            testCase.client().request.for(resource);
+            testCase.client().request.conditions({ ims: resource.notModifiedSince() });
+            testCase.check(() => {
+                testCase.expectStatusCode(304);
+            });
+            await testCase.run();
+        }
+
+        let updatingResponse = null; // TBD
+        {
+            let testCase = new ProxyCase('miss and get a 304 that updates the previously cached response');
+
+            resource.modifyNow();
+            resource.expireAt(FuzzyTime.DistantFuture());
+
+            testCase.client().request.for(resource);
+            testCase.client().request.conditions({ ims: resource.modifiedSince() });
+            testCase.client().request.header.add("Cache-Control", "max-age=0");
+
+            testCase.server().serve(resource);
+            testCase.server().response.tag("second");
+            testCase.server().response.startLine.statusCode = 304;
+
+            testCase.check(() => {
+                testCase.expectStatusCode(200);
+                // XXX: Check the headers.
+                updatingResponse = testCase.server().transaction().response;
+            });
+
+            await testCase.run();
+        }
+
+        {
+            let testCase = new ProxyCase('hit updated headers');
+            testCase.client().request.for(resource);
+            testCase.check(() => {
+                testCase.expectStatusCode(200);
+                let updatedResponse = testCase.client().transaction().response;
+                assert.equal(updatedResponse.tag(), updatingResponse.tag(), "updated X-Daft-Response-Tag");
+                assert.equal(updatedResponse.id(), updatingResponse.id(), "updated X-Daft-Response-ID");
+                assert.equal(updatedResponse.header.values("Last-Modified"), resource.lastModificationTime.toUTCString(), "updated Last-Modified");
+                assert.equal(updatedResponse.header.values("Expires"), resource.nextModificationTime.toUTCString(), "updated Expires");
+                assert.equal(updatedResponse.header.value(hitCheck.name), hitCheck.value, "preserved originally cached header field");
+            });
+            await testCase.run();
+        }
+
+        {
+            let testCase = new ProxyCase('cleanup leftovers using a cachable response');
+            resource.uri.makeUnique();
+            testCase.client().request.for(resource);
+            testCase.client().request.tag("cleanup");
+            testCase.server().serve(resource);
+            testCase.server().response.tag("cleanup");
+            await testCase.run();
+        }
+
+        AddressPool.ReleaseListeningAddress(resource.uri.address);
     }
-
-    {
-        let testCase = new ProxyCase('respond with a 304 hit');
-        testCase.client().request.for(resource);
-        testCase.client().request.conditions({ ims: resource.notModifiedSince() });
-        testCase.check(() => {
-            testCase.expectStatusCode(304);
-        });
-        await testCase.run();
-    }
-
-    let updatingResponse = null; // TBD
-    {
-        let testCase = new ProxyCase('miss and get a 304 that updates the previously cached response');
-
-        resource.modifyNow();
-        resource.expireAt(FuzzyTime.DistantFuture());
-
-        testCase.client().request.for(resource);
-        testCase.client().request.conditions({ ims: resource.modifiedSince() });
-        testCase.client().request.header.add("Cache-Control", "max-age=0");
-
-        testCase.server().serve(resource);
-        testCase.server().response.tag("second");
-        testCase.server().response.startLine.statusCode = 304;
-
-        testCase.check(() => {
-            testCase.expectStatusCode(200);
-            // XXX: Check the headers.
-            updatingResponse = testCase.server().transaction().response;
-        });
-
-        await testCase.run();
-    }
-
-    {
-        let testCase = new ProxyCase('hit updated headers');
-        testCase.client().request.for(resource);
-        testCase.check(() => {
-            testCase.expectStatusCode(200);
-            let updatedResponse = testCase.client().transaction().response;
-            assert.equal(updatedResponse.tag(), updatingResponse.tag(), "updated X-Daft-Response-Tag");
-            assert.equal(updatedResponse.id(), updatingResponse.id(), "updated X-Daft-Response-ID");
-            assert.equal(updatedResponse.header.values("Last-Modified"), resource.lastModificationTime.toUTCString(), "updated Last-Modified");
-            assert.equal(updatedResponse.header.values("Expires"), resource.nextModificationTime.toUTCString(), "updated Expires");
-            assert.equal(updatedResponse.header.value(hitCheck.name), hitCheck.value, "preserved originally cached header field");
-        });
-        await testCase.run();
-    }
-
-    {
-        let testCase = new ProxyCase('cleanup leftovers using a cachable response');
-        resource.uri.makeUnique();
-        testCase.client().request.for(resource);
-        testCase.client().request.tag("cleanup");
-        testCase.server().serve(resource);
-        testCase.server().response.tag("cleanup");
-        await testCase.run();
-    }
-
-    AddressPool.ReleaseListeningAddress(resource.uri.address);
-}
 
 }
