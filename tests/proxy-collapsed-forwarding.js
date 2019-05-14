1b29c02c7dd950c0c3bc086846f8ce7dc3f20173
Alex Rousskov <rousskov@measurement-factory.com>
2019-05-14 10:20:49 -0600

    tests/proxy-collapsed-forwarding: Use DUT overlord, one HttpCase, and
    ... more flexible/precise workers configuration.
    
    Basic collapsed forwarding testing uses N clients feeding off one origin
    server response. A single HttpCase instance supports this scenario.
    
    The new transaction inter-dependencies APIs simplify configuring those
    dependencies.

--- tests/proxy-collapsed-forwarding.js
+++ tests/proxy-collapsed-forwarding.js
@@ -5,6 +5,7 @@
 /* Tests whether an HTTP proxy caches a response
  * Parameters: [drop-Content-Length] [body size] */
 
+import assert from "assert";
 import HttpTestCase from "../src/test/HttpCase";
 import Body from "../src/http/Body";
 import Resource from "../src/anyp/Resource";
@@ -12,30 +13,37 @@ import * as Gadgets from "../src/misc/Gadgets";
 import * as Config from "../src/misc/Config";
 import * as AddressPool from "../src/misc/AddressPool";
 import { Must } from "../src/misc/Gadgets";
-import assert from "assert";
 import Test from "../src/test/Test";
+import { DutConfig, ProxyOverlord } from "../src/overlord/Proxy";
 
-const BasePort = Config.ProxyListeningAddress.port;
-const Host = Config.ProxyListeningAddress.host;
-const AddrLimit = 8;
-let ProxyListeningAddresses = [];
-for (let i = 0; i < AddrLimit; ++i) {
-    ProxyListeningAddresses.push({host: Host, port: BasePort + i});
+// Compute syntax specification for the --collapsed-requests option.
+// The WorkerLimit is only needed to keep --help output reasonable.
+// TODO: Improve --collapsed-requests syntax specification and/or help.
+const WorkerLimit = 8;
+let CollapsedRequestsType = "{each:Maybe Number";
+for (let i = 0; i < WorkerLimit; ++i) {
+    CollapsedRequestsType += `,${i+1}:Maybe Number`;
 }
+CollapsedRequestsType += "}";
 
 Config.Recognize([
-    {
-        option: "requests",
-        type: "Number",
-        default: "4",
-        description: "simultaneous requests number (>1 && <="+AddrLimit,
-    },
     {
         option: "workers",
         type: "Number",
         default: "4",
-        description: "Squid workers number",
+        description: "the number of Squid SMP workers",
+        // TODO: Validate: workers <= WorkerLimit.
     },
+    {
+        // The miss transaction always goes through the first worker.
+        // Collapsed requests go to the workers mapped by this option.
+        // For example, to send 2 collapsed requests to each worker
+        // but keep the third worker idle, use --collapsed-requests each:2,3:0
+        option: "collapsed-requests",
+        type: CollapsedRequestsType,
+        default: "{each:2}",
+        description: "the number of collapsed requests for each worker",
+    }
 ]);
 
 export default class MyTest extends Test {
@@ -43,101 +51,88 @@ export default class MyTest extends Test {
     constructor(...args) {
         // XXX: We should not be writing constructors to configure a DUT.
         // TODO: Add virtual Test::configureDut() or a similar method.
-        super(null, ...args); // no DUT for now
+        const cfg = new DutConfig();
+        cfg.workers(Config.Workers);
+        cfg.dedicatedWorkerPorts(true);
+        cfg.collapsedForwarding(true);
+        cfg.memoryCaching(true); // TODO: And/or disk caching (via Config).
+        super(new ProxyOverlord(cfg), ...args); // no DUT for now
+
+        this._workerListeningAddresses = cfg.workerListeningAddresses();
     }
 
     async run(/*testRun*/) {
 
-    Must(Config.Requests > 1);
+        // TODO: Zero workers should mean non-SMP mode.
+        Must(Config.Workers >= 1);
 
-    // TODO: Remove args. Use Config directly instead.
-    const args = {
-        workers: Config.Workers,
-        requests: Config.Requests
-    };
-
-    let resource = new Resource();
-    resource.makeCachable();
-    resource.uri.address = AddressPool.ReserveListeningAddress();
-    resource.body = new Body(Gadgets.UniqueId("body-"));
-    resource.finalize();
-
-    const initiatorAddr = ProxyListeningAddresses[0];
-    const smp = args.workers > 1;
-    const collapseAddr = smp ? ProxyListeningAddresses[1] : ProxyListeningAddresses[0];
-
-    // need this for pids checking for SMP
-    let pids = [2];
-
-    let collapseCase = new HttpTestCase('collapse on the existing transaction', collapseAddr);
-    collapseCase.client().request.for(resource);
-    collapseCase.client().nextHopAddress = collapseAddr;
-    let requests = args.requests - 2;
-    if (requests > 0) {
-        for (let i = 2; i < args.workers; ++i) {
-            pids.push(i+1);
-            if (--requests === 0)
-                break;
-        }
-        for (let clientId = 1; clientId <= requests; ++clientId) {
-            const lastPid = pids[pids.length-1];
-            pids.push(lastPid);
-
-            let client = collapseCase.addClient();
-            const lastAddress = ProxyListeningAddresses[args.workers - 1]; // XXX: Why that? Why does not it change with the request/worker?
-            client.nextHopAddress = lastAddress;
+        // convert Config.CollapsedRequests (which may not specify some
+        // workers and/or have an "each" macro) into collapsedRequestsForWorker
+        const defaultRequests = 'each' in Config.CollapsedRequests ?
+                Config.CollapsedRequests.each : 0;
+        let collapsedRequestsForWorker = Array(1+Config.Workers).fill(defaultRequests);
+        for (let workerLabel of Object.keys(Config.CollapsedRequests)) {
+            if (workerLabel === "each") // handled above
+                continue;
+            assert(workerLabel > 0);
+            assert(workerLabel <= WorkerLimit);
+            assert(workerLabel <= Config.Workers); // a worker ID in --collapsed-requests list exceeded --workers value
+            collapsedRequestsForWorker[workerLabel] = Config.CollapsedRequests[workerLabel];
         }
 
-    }
-    collapseCase.check(() => {
-        let pidIdx = 0;
-        for (let client of collapseCase.clients()) {
+        let resource = new Resource();
+        resource.makeCachable();
+        resource.uri.address = AddressPool.ReserveListeningAddress();
+        resource.body = new Body(Gadgets.UniqueId("body-"));
+        resource.finalize();
+
+        let testCase = new HttpTestCase('one and only'); // TODO: Use a testRun-based label
+        testCase.server().serve(resource);
+        testCase.server().response.tag("first");
+        let missClient = testCase.client();
+        missClient.request.for(resource);
+        missClient.nextHopAddress = this._workerListeningAddresses[1];
+        missClient.expectStatusCode(200);
+        missClient.checks.add((client) => {
             assert(client.transaction().response, "Proxy must send a response");
-            const collapseStatus = client.transaction().response.startLine.statusNumber();
-            if (smp) {
-                const pid = client.transaction().response.proxyPid();
-                assert.equal(pid, pids[pidIdx], "expected worker pid");
-                pidIdx++;
-            }
-            const collapseTag = client.transaction().response.tag();
-            assert.equal(collapseStatus, 200, "expected response status code 200");
-            assert.equal(collapseTag, "first", "Squid collapsed worker X-Daft-Response-Tag");
+            const initiatorTag = client.transaction().response.tag();
+            assert.equal(initiatorTag, "first", "Squid collapsing initiator worker X-Daft-Response-Tag XXX");
+            let responseBody = client.transaction().response.body.whole();
+            // XXX: Don't we have a message comparison function?
+            assert.equal(responseBody, resource.body.whole(), "Got response body");
+        });
+
+        // add clients for each worker; they should all collapse on missClient
+        for (let worker = 1; worker <= Config.Workers; ++worker) {
+            testCase.makeClients(collapsedRequestsForWorker[worker], (hitClient) => {
+                hitClient.request.for(resource);
+                hitClient.nextHopAddress = this._workerListeningAddresses[worker];
+
+                // technically, reaching the proxy is enough, but we cannot detect/wait for that
+                hitClient.transaction().blockSendingUntil(
+                        testCase.server().transaction().receivedEverything(),
+                        "wait for the miss request to reach the server");
+
+                hitClient.expectStatusCode(200);
+                hitClient.checks.add((client) => {
+                    // XXX: Revise hit checks. Remove duplication. Move to a method?
+                    assert(client.transaction().response, "Proxy must send a response");
+                    const initiatorTag = client.transaction().response.tag();
+                    assert.equal(initiatorTag, "first", "Squid collapsing initiator worker X-Daft-Response-Tag XXX");
+                    let responseBody = client.transaction().response.body.whole();
+                    // XXX: Don't we have a message comparison function?
+                    assert.equal(responseBody, resource.body.whole(), "Got response body");
+                });
+            });
         }
-    });
 
+        testCase.server().transaction().blockSendingUntil(
+            testCase.clientsSentEverything(),
+            "wait for all clients to collapse");
 
-    let initiatorCase = new HttpTestCase('initiate a transaction', initiatorAddr);
-    initiatorCase.client().request.for(resource);
-    initiatorCase.check(() => {
-        assert(initiatorCase.client().transaction().response, "Proxy must send a response");
-        initiatorCase.expectStatusCode(200);
-        //const initiatorPid = initiatorCase.client().transaction().response.proxyPid();
-        const initiatorTag = initiatorCase.client().transaction().response.tag();
-        assert.equal(initiatorTag, "first", "Squid collapsing initiator worker X-Daft-Response-Tag");
-        let responseBody = initiatorCase.client().transaction().response.body.whole();
-        assert.equal(responseBody, resource.body.whole(), "Served response body");
-    });
+        await testCase.run();
 
-    let serverCase = new HttpTestCase('serve a response');
-    serverCase.server().response.tag("first");
-    serverCase.serverWillWaitForClients([collapseCase]);
-    serverCase.server().serve(resource);
-
-    let collapsePromise = null;
-    serverCase.server().response.promiseToReceive().then(() => {
-        serverCase.server().response.tag("second");
-        serverCase.server().response.promiseToReceive(false);
-        collapsePromise = collapseCase.run();
-    });
-
-    let serverPromise = serverCase.run();
-    let initiatorPromise = initiatorCase.run();
-    await initiatorPromise;
-    assert(collapsePromise);
-    await collapsePromise;
-    await serverPromise;
-
-    AddressPool.ReleaseListeningAddress(resource.uri.address);
+        AddressPool.ReleaseListeningAddress(resource.uri.address);
 }
 
 }
