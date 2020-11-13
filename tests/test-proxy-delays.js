860bbf33a09616f70c0ef2e1cee3ca79a98d5bd6
Alex Rousskov <rousskov@measurement-factory.com>
2020-11-13 16:55:59 -0500

    tests/test-proxy-delays: Modernize
    ... including running both "slow" and "fast" test cases by default.
    
    Also use official response_delay_pool option names.
    
    Requires Daft commit 7fed4f1.
    
    Use --ignore-all-space to reduce diff noise.

--- tests/test-proxy-delays.js
+++ tests/test-proxy-delays.js
@@ -3,129 +3,138 @@
 // http://www.measurement-factory.com/
 
 // Check whether proxy-introduced delays match proxy configuration.
-// The current focus is on Squid's response_delay_pool directive.
-
-// Expected Squid response delay pool configuration:
-//
-// acl slowResponse rep_header Speed ^slow
-// response_delay_pool slowPool \
-//     bucket_speed_limit=50000 \
-//     max_bucket_size=100000 \
-//     aggregate_speed_limit=100000 \
-//     max_aggregate_size=200000 \
-//     initial_fill_level=90
-// response_delay_pool_access slowPool allow slowResponse
-
+// TODO: Test more than just response_delay_pool directive.
 
 import Promise from "bluebird";
 import HttpTestCase from "../src/test/HttpCase";
 import Body from "../src/http/Body";
 import Resource from "../src/anyp/Resource";
+import * as AddressPool from "../src/misc/AddressPool";
 import * as Http from "../src/http/Gadgets";
 import * as Gadgets from "../src/misc/Gadgets";
 import * as Config from "../src/misc/Config";
-import StartTests from "../src/test/Runner";
+import Test from "../src/overlord/Test";
+import ConfigGen from "../src/test/ConfigGen";
 import assert from "assert";
-
-process.on("unhandledRejection", function (reason /*, promise */) {
-    console.log("Quitting on a rejected promise:", reason);
-    throw reason;
-});
-Promise.config({ warnings: true });
 
 Config.Recognize([
     {
+        // TODO: Rename.
         option: "speed",
         type: "String",
         enum: ["fast", "slow"],
         default: "slow",
-        description: "expected response speed",
+        description: "whether to test using 'slow' transactions " +
+            "(i.e. transactions that DUT should delay)",
     },
     {
         option: "bucket-speed-limit",
         type: "Number",
         default: "50000",
-        description: "response_delay_pool's bucket_speed_limit",
+        description: "response_delay_pool's individual-restore",
     },
     {
         option: "aggregate-speed-limit",
         type: "Number",
-        description: "response_delay_pool's aggregate_speed_limit",
+        description: "response_delay_pool's aggregate-restore",
     }
 ]);
 
-// TODO: Change --body-size default:
-// Config.Customize("body-size", {
-//     default: srvBodySize,
-// });
-const srvBodySize = 1000 * 1000; // 1 MB
 const maxDeviation = 10.0; // percent
 
-async function Test(testRun, callback) {
+export default class MyTest extends Test {
+    static Configurators() {
+        assert(Config.BodySize >= 0, "positive body-size"); // TODO: Add Size option type
 
-    if (Config.AggregateSpeedLimit === undefined)
-        Config.AggregateSpeedLimit = Config.BucketSpeedLimit * Config.ConcurrencyLevel;
+        const configGen = new ConfigGen();
 
-    // do not log large body handling details by default
-    if (Config.LogBodies === undefined)
-        Config.LogBodies = 0;
+        // custom default
+        configGen.addGlobalConfigVariation({bodySize: [
+            1024 * 1024, // 1 MB
+        ]});
 
-    let slowSpeed = Config.AggregateSpeedLimit / Config.ConcurrencyLevel;
-    if (slowSpeed > Config.BucketSpeedLimit)
-        slowSpeed = Config.BucketSpeedLimit;
+        // dynamic default
+        configGen.addGlobalConfigVariation({aggregateSpeedLimit: [
+            Config.BucketSpeedLimit * Config.ConcurrencyLevel
+        ]});
 
-    let expectedSpeed;
-    let description;
-    if (Config.Speed === "slow") {
-        expectedSpeed = slowSpeed;
-        description = 'Expecting ' + expectedSpeed + ' byte/s response transmission';
-    } else {
-        expectedSpeed = slowSpeed * 2; // XXX: minimum speed, actually
-        description = 'Expecting very fast response transmission';
+        configGen.addGlobalConfigVariation({speed: [
+            "slow",
+            "fast",
+        ]});
+
+        return configGen.generateConfigurators();
     }
 
-    let resource = new Resource();
-    resource.uri.address = Gadgets.ReserveListeningAddress();
-    resource.uri.makeUnique("/speed=" + Config.Speed + "/");
-    resource.body = new Body(Gadgets.RandomText(srvBodySize));
 
-    let testCase = new HttpTestCase(description);
-    testCase.client().request.for(resource);
-    testCase.server().serve(resource);
-    testCase.server().response.header.add("Speed", Config.Speed);
+    _configureDut(cfg) {
+        cfg.custom('acl slowResponse rep_header Speed ^slow');
+        cfg.custom('response_delay_pool slowPool ' +
+                   `individual-restore=${Config.BucketSpeedLimit} ` +
+                   'individual-maximum=100000 ' +
+                   `aggregate-restore=${Config.AggregateSpeedLimit} ` +
+                   'aggregate-maximum=200000 ' +
+                   'initial-bucket-level=90');
+        cfg.custom('response_delay_pool_access slowPool allow slowResponse');
+    }
 
-    // XXX: startDate and endDate should be reported by transactions and
-    // renamed to startTime() and stopTime().
-    const startDate = new Date();
-    testCase.check(() => {
-        const endDate = new Date();
 
-        testCase.expectStatusCode(200);
-        Http.AssertForwardedMessage(testCase.server().transaction().response,
-            testCase.client().transaction().response, "forwarded response");
+    async run(/*testRun*/) {
+        // do not log large body handling details by default
+        if (Config.LogBodies === undefined && Config.BodySize > 1*1024*1024)
+            Config.LogBodies = 0;
 
-        const elapsedSec = (endDate.getTime() - startDate.getTime()) / 1000.0;
-        console.log("Transaction took " + elapsedSec.toFixed(1) + "s");
-        const speed = srvBodySize / elapsedSec;
+        let slowSpeed = Config.AggregateSpeedLimit / Config.ConcurrencyLevel;
+        if (slowSpeed > Config.BucketSpeedLimit)
+            slowSpeed = Config.BucketSpeedLimit;
+
+        let expectedSpeed;
+        let description;
         if (Config.Speed === "slow") {
-            const deviation = 100. * Math.abs(speed - expectedSpeed) / expectedSpeed;
-            console.log("Actual speed=%d byte/s; deviation (from the expected %d byte/s) is %d%%",
-                speed.toFixed(0), expectedSpeed.toFixed(0), deviation.toFixed(1));
-            assert(deviation <= maxDeviation);
+            expectedSpeed = slowSpeed;
+            description = 'Expecting ' + expectedSpeed + ' byte/s response transmission';
         } else {
-            console.log("Actual speed=%d byte/s; expected at least %d byte/s",
-                speed.toFixed(0), expectedSpeed.toFixed(0));
-            assert(speed >= expectedSpeed);
+            expectedSpeed = slowSpeed * 2; // XXX: minimum speed, actually
+            description = 'Expecting very fast response transmission';
         }
 
-        // TODO: Test other response_delay_pool parameters as well.
-    });
+        let resource = new Resource();
+        resource.uri.address = AddressPool.ReserveListeningAddress();
+        resource.uri.makeUnique("/speed=" + Config.Speed + "/");
+        resource.body = new Body(Gadgets.RandomText("body-", Config.BodySize));
 
-    await testCase.run();
+        let testCase = new HttpTestCase(description);
+        testCase.client().request.for(resource);
+        testCase.server().serve(resource);
+        testCase.server().response.header.add("Speed", Config.Speed);
 
-    console.log("Test result: success");
-    if (callback)
-        callback();
+        testCase.addMissCheck();
+        testCase.check(() => {
+            testCase.expectStatusCode(200);
+
+            const startDate = testCase.server().transaction().startTime();
+            const endDate = testCase.client().transaction().receivedEverythingTime();
+
+            const elapsedSec = (endDate.getTime() - startDate.getTime()) / 1000.0;
+            console.log("Transaction took " + elapsedSec.toFixed(1) + "s");
+            const speed = Config.BodySize / elapsedSec;
+            if (Config.Speed === "slow") {
+                const deviation = 100. * Math.abs(speed - expectedSpeed) / expectedSpeed;
+                console.log("Actual speed=%d byte/s; deviation (from the expected %d byte/s) is %d%%",
+                    speed.toFixed(0), expectedSpeed.toFixed(0), deviation.toFixed(1));
+                assert(deviation <= maxDeviation);
+            } else {
+                console.log("Actual speed=%d byte/s; expected at least %d byte/s",
+                    speed.toFixed(0), expectedSpeed.toFixed(0));
+                assert(speed >= expectedSpeed);
+            }
+
+            // TODO: Test other response_delay_pool parameters as well.
+        });
+
+        await testCase.run();
+
+        AddressPool.ReleaseListeningAddress(resource.uri.address);
+    }
+
 }
-
-StartTests(Test);
