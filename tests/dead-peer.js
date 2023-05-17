7553b6bec6a15cc1d9b3dfd1b2ae322c0678cc28
Alex Rousskov <rousskov@measurement-factory.com>
2023-05-17 11:52:25 -0400

    tests/dead-peer: Support GET-direct-to-dead (and GET-through-to-dead)
        GET     directly to a non-listening origin
        GET     through a cache_peer to a non-listening origin
        CONNECT through a cache_peer to a non-listening origin
        GET     through a non-listening cache_peer
        CONNECT through a non-listening cache_peer
    
    Requires --dut-cache-peers support in Daft (currently Daft feature
    branch commit b3a2b92).

--- tests/dead-peer.js
+++ tests/dead-peer.js
@@ -2,8 +2,7 @@
 // Copyright (C) The Measurement Factory.
 // http://www.measurement-factory.com/
 
-// Checks handling of a failed transaction involving a cache_peer.
-// TODO: Refactoring into more general forwarding-failures?
+// Checks handling of transactions that fail due to non-listening peers.
 
 import * as AddressPool from "../src/misc/AddressPool";
 import * as Config from "../src/misc/Config";
@@ -13,20 +12,13 @@ import Resource from "../src/anyp/Resource";
 import Test from "../src/overlord/Test";
 import { FlexibleConfigGen } from "../src/test/ConfigGen";
 
-Config.Recognize([
-    {
-        option: "cache-peers",
-        type: "Number",
-        description: "the number of proxy cache_peers to use",
-    },
-]);
-
 export default class MyTest extends Test {
 
     static Configurators() {
         const configGen = new FlexibleConfigGen();
 
-        configGen.cachePeers(function *() {
+        configGen.dutCachePeers(function *() {
+            yield 0;
             yield 1;
             yield 2;
         });
@@ -34,24 +26,47 @@ export default class MyTest extends Test {
         return configGen.generateConfigurators();
     }
 
-    constructor() {
-        super(...arguments);
+    async testGetDirectlyToBadOrigin() {
+        const originAddress = AddressPool.ReserveListeningAddress();
+
+        let testCase = new HttpTestCase('GET directly to a non-listening origin');
+        testCase.client().request.startLine.uri.address = originAddress;
+        // no server to simulate an origin that is not listening
+
+        testCase.check(() => {
+            testCase.expectStatusCode(503);
+            // TODO: Check access.log fields
+        });
+
+        await testCase.run();
+
+        AddressPool.ReleaseListeningAddress(originAddress);
     }
 
-    _configureDut(cfg) {
-        cfg.withCachePeers(Config.cachePeers());
+    async testGetThroughCachePeerToBadOrigin() {
+        const originAddress = AddressPool.ReserveListeningAddress();
+
+        let testCase = new HttpTestCase('GET through a cache_peer to a non-listening origin');
+        testCase.client().request.startLine.uri.address = originAddress;
+        // no server to simulate an origin that is not listening
+
+        this._configureCachePeersTalkingToBadOrigin();
+
+        CachePeer.Attract(testCase.client().request);
+
+        testCase.check(() => {
+            testCase.expectStatusCode(503);
+            // TODO: Check access.log fields
+        });
+
+        await testCase.run();
+
+        AddressPool.ReleaseListeningAddress(originAddress);
     }
 
     async testConnectThroughCachePeerToBadOrigin() {
-        let testCase = new HttpTestCase('CONNECT to a non-listening origin through a cache_peer');
-
-        // simulate what a cache_peer does when the origin is not listening
-        this.dut.cachePeers().forEach(cachePeer => {
-            cachePeer.resetTransaction();
-            cachePeer.response.startLine.code(503);
-            cachePeer.response.header.add("Via",
-                `1.1 ${cachePeer.context.id} (Daft-cache_peer)`);
-        });
+        let testCase = new HttpTestCase('CONNECT through a cache_peer to a non-listening origin');
+        this._configureCachePeersTalkingToBadOrigin();
 
         testCase.client().request.startLine.uri.address = {
             host: Config.originAuthority().host,
@@ -92,7 +107,7 @@ export default class MyTest extends Test {
     }
 
     async testConnectThroughBadCachePeer() {
-        let testCase = new HttpTestCase('CONNECT to a non-listening cache_peer');
+        let testCase = new HttpTestCase('CONNECT through a non-listening cache_peer');
 
         testCase.client().request.startLine.uri.address = {
             host: Config.originAuthority().host,
@@ -109,7 +124,26 @@ export default class MyTest extends Test {
         await testCase.run();
     }
 
+    // simulate what a cache_peer does when the origin is not listening
+    async _configureCachePeersTalkingToBadOrigin() {
+        this.dut.cachePeers().forEach(cachePeer => {
+            cachePeer.resetTransaction();
+            cachePeer.response.startLine.code(503);
+            cachePeer.response.header.add("Via",
+                `1.1 ${cachePeer.context.id} (Daft-cache_peer)`);
+        });
+    }
+
     async run(/*testRun*/) {
+        // TODO: Add generic Config support for selecting which cases to run
+        // based on their MyTest::testX() method name suffixes.
+
+        if (!Config.dutCachePeers()) {
+            await this.testGetDirectlyToBadOrigin();
+            return; // the other test cases require at least one cache_peer
+        }
+
+        await this.testGetThroughCachePeerToBadOrigin();
         await this.testConnectThroughCachePeerToBadOrigin();
 
         // the remaining test cases need cache_peers that do not listen
