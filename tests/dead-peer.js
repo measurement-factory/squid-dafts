b81d5087b65ca17be3e3e45c775f52504228f213
Alex Rousskov <rousskov@measurement-factory.com>
2023-05-18 15:06:57 -0400

    tests/dead-peer: Check access.log records
    Requires Daft support for getting access.log records from Squid
    (currently Daft feature branch commit 9e61c43)

--- tests/dead-peer.js
+++ tests/dead-peer.js
@@ -4,6 +4,8 @@
 
 // Checks handling of transactions that fail due to non-listening peers.
 
+import assert from "assert";
+
 import * as AddressPool from "../src/misc/AddressPool";
 import * as Config from "../src/misc/Config";
 import * as CachePeer from "../src/overlord/CachePeer";
@@ -33,9 +35,17 @@ export default class MyTest extends Test {
         testCase.client().request.startLine.uri.address = originAddress;
         // no server to simulate an origin that is not listening
 
-        testCase.check(() => {
+        testCase.check(async () => {
             testCase.expectStatusCode(503);
-            // TODO: Check access.log fields
+            const accessRecords = await this.dut.getNewAccessRecords();
+            const accessRecord = accessRecords.single();
+            accessRecord.checkEqual('%err_code', 'ERR_CONNECT_FAIL');
+            accessRecord.checkEqual('%err_detail', 'WITH_SERVER+errno=111');
+            accessRecord.checkEqual('%Ss', 'TCP_MISS_ABORTED');
+            accessRecord.checkEqual('%>Hs', '503');
+            accessRecord.checkEqual('%rm', 'GET');
+            accessRecord.checkEqual('%Sh', 'HIER_DIRECT');
+            accessRecord.checkKnown('%<a');
         });
 
         await testCase.run();
@@ -54,9 +64,17 @@ export default class MyTest extends Test {
 
         CachePeer.Attract(testCase.client().request);
 
-        testCase.check(() => {
+        testCase.check(async () => {
             testCase.expectStatusCode(503);
-            // TODO: Check access.log fields
+            const accessRecords = await this.dut.getNewAccessRecords();
+            const accessRecord = accessRecords.single();
+            accessRecord.checkUnknown('%err_code');
+            accessRecord.checkUnknown('%err_detail');
+            accessRecord.checkEqual('%Ss', 'TCP_MISS');
+            accessRecord.checkEqual('%>Hs', '503');
+            accessRecord.checkEqual('%rm', 'GET');
+            accessRecord.checkEqual('%Sh', 'FIRSTUP_PARENT');
+            accessRecord.checkKnown('%<a');
         });
 
         await testCase.run();
@@ -75,9 +93,17 @@ export default class MyTest extends Test {
         testCase.client().request.startLine.method = 'CONNECT';
         CachePeer.Attract(testCase.client().request);
 
-        testCase.check(() => {
+        testCase.check(async () => {
             testCase.expectStatusCode(503);
-            // TODO: Check access.log fields
+            const accessRecords = await this.dut.getNewAccessRecords();
+            const accessRecord = accessRecords.single();
+            accessRecord.checkEqual('%err_code', 'ERR_RELAY_REMOTE');
+            accessRecord.checkUnknown('%err_detail');
+            accessRecord.checkEqual('%Ss', 'TCP_TUNNEL');
+            accessRecord.checkEqual('%>Hs', '503');
+            accessRecord.checkEqual('%rm', 'CONNECT');
+            accessRecord.checkEqual('%Sh', 'FIRSTUP_PARENT');
+            accessRecord.checkKnown('%<a');
         });
 
         await testCase.run();
@@ -96,9 +122,17 @@ export default class MyTest extends Test {
         CachePeer.Attract(testCase.client().request);
         testCase.server().serve(resource);
 
-        testCase.check(() => {
+        testCase.check(async () => {
             testCase.expectStatusCode(503);
-            // TODO: Check access.log fields
+            const accessRecords = await this.dut.getNewAccessRecords();
+            const accessRecord = accessRecords.single();
+            accessRecord.checkEqual('%err_code', 'ERR_CONNECT_FAIL');
+            accessRecord.checkEqual('%err_detail', 'WITH_SERVER+errno=111');
+            accessRecord.checkEqual('%Ss', 'TCP_MISS_ABORTED');
+            accessRecord.checkEqual('%>Hs', '503');
+            accessRecord.checkEqual('%rm', 'GET');
+            accessRecord.checkEqual('%Sh', this._expectedBadPeerHierarchyStatus());
+            accessRecord.checkKnown('%<a');
         });
 
         await testCase.run();
@@ -116,9 +150,17 @@ export default class MyTest extends Test {
         testCase.client().request.startLine.method = 'CONNECT';
         CachePeer.Attract(testCase.client().request);
 
-        testCase.check(() => {
+        testCase.check(async () => {
             testCase.expectStatusCode(503);
-            // TODO: Check access.log fields
+            const accessRecords = await this.dut.getNewAccessRecords();
+            const accessRecord = accessRecords.single();
+            accessRecord.checkEqual('%err_code', 'ERR_CONNECT_FAIL');
+            accessRecord.checkEqual('%err_detail', 'errno=111');
+            accessRecord.checkEqual('%Ss', 'TCP_TUNNEL');
+            accessRecord.checkEqual('%>Hs', '503');
+            accessRecord.checkEqual('%rm', 'CONNECT');
+            accessRecord.checkEqual('%Sh', this._expectedBadPeerHierarchyStatus());
+            accessRecord.checkKnown('%<a');
         });
 
         await testCase.run();
@@ -134,6 +176,13 @@ export default class MyTest extends Test {
         });
     }
 
+    // expected %Sh value for cases testing non-listening cache_peers
+    _expectedBadPeerHierarchyStatus() {
+        const peers = Config.dutCachePeers();
+        assert(peers);
+        return peers > 1 ? 'ANY_OLD_PARENT' : 'FIRSTUP_PARENT';
+    }
+
     async run(/*testRun*/) {
         // TODO: Add generic Config support for selecting which cases to run
         // based on their MyTest::testX() method name suffixes.
@@ -143,14 +192,16 @@ export default class MyTest extends Test {
             return; // the other test cases require at least one cache_peer
         }
 
+        // cache_peers that return 503s may generate ERRORs;
+        // cache_peers that do not listen may generate ERRORs
+        this.dut.ignoreProblems(/Connection to peer.* failed/);
+
         await this.testGetThroughCachePeerToBadOrigin();
         await this.testConnectThroughCachePeerToBadOrigin();
 
         // the remaining test cases need cache_peers that do not listen
         await this.dut.stopCachePeers();
 
-        // cache_peers that do not listen may generate ERRORs
-        this.dut.ignoreProblems(/Connection to peer.*failed/);
 
         await this.testGetThroughBadCachePeer();
         await this.testConnectThroughBadCachePeer();
