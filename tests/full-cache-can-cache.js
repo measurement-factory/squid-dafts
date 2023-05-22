804ba8170cee8729dcf61600b0203c8f5a8f55a4
Eduard Bagdasaryan <dictiano@gmail.com>
2023-05-22 17:37:47 +0300

    A test checking that a refilled cached entry is not available any more

--- tests/full-cache-can-cache.js
+++ tests/full-cache-can-cache.js
@@ -39,6 +39,14 @@ Config.Recognize([
 
 export default class MyTest extends Test {
 
+    constructor(...args) {
+        super(...args);
+        // 1. cache this URL at the beginning
+        // 2. refill the cache and expect this entry to be purged due to key collision
+        // 3. request this URL again and expect a miss
+        this.cachedUrl = null;
+    }
+
     _configureDut(cfg) {
 
         if (Config.CacheType === 'disk')
@@ -60,6 +68,36 @@ export default class MyTest extends Test {
         return configGen.generateConfigurators();
     }
 
+    async doMiss(resource, step, description) {
+        let missCase = new HttpTestCase(`${description}: forward a ${ResponseBodyBytes}-byte response`);
+        missCase.server().serve(resource);
+        missCase.server().response.forceEof = Config.ResponseEndsAtEof;
+        missCase.client().request.for(resource);
+        if (Config.Smp)
+            missCase.client().nextHopAddress = this._workerListeningAddresses[step];
+        missCase.addMissCheck();
+        await missCase.run();
+
+        await this.dut.finishCaching();
+        return missCase;
+    }
+
+    async doHit(resource, missCase, step, description) {
+        let hitCase = new HttpTestCase(`${description}: hit a ${ResponseBodyBytes}-byte response`);
+        hitCase.client().request.for(resource);
+        if (Config.Smp)
+            hitCase.client().nextHopAddress = this._workerListeningAddresses[step];
+        hitCase.addHitCheck(missCase.server().transaction().response);
+        try {
+            await hitCase.run();
+            return 1;
+        } catch (ex) {
+            const response = hitCase.client().transaction().response;
+            assert(response.startLine.codeInteger() === 503);
+        }
+        return 0;
+    }
+
     async testStep(step, description) {
         const address = AddressPool.ReserveListeningAddress();
         let hits = 0;
@@ -69,34 +107,29 @@ export default class MyTest extends Test {
             resource.makeCachable();
             resource.body = new Body('x'.repeat(ResponseBodyBytes));
             resource.finalize();
-
-            let missCase = new HttpTestCase(`${description}: forward a ${ResponseBodyBytes}-byte response`);
-            missCase.server().serve(resource);
-            missCase.server().response.forceEof = Config.ResponseEndsAtEof;
-            missCase.client().request.for(resource);
-            if (Config.Smp)
-                missCase.client().nextHopAddress = this._workerListeningAddresses[step];
-            missCase.addMissCheck();
-            await missCase.run();
-
-            await this.dut.finishCaching();
-
-            let hitCase = new HttpTestCase(`${description}: hit a ${ResponseBodyBytes}-byte response`);
-            hitCase.client().request.for(resource);
-            if (Config.Smp)
-                hitCase.client().nextHopAddress = this._workerListeningAddresses[step];
-
-            hitCase.addHitCheck(missCase.server().transaction().response);
-
-            try {
-                await hitCase.run();
-                hits++;
-            } catch (ex) {
-                const response = hitCase.client().transaction().response;
-                assert(response.startLine.codeInteger() === 503);
+            if (this.cachedUrl === null) {
+                this.cachedUrl = resource.uri;
+//                AddressPool.ReleaseListeningAddress(address);
+//                return;
             }
+            let missCase = await this.doMiss(resource, step, description);
+            hits += await this.doHit(resource, missCase, step, description);
         }
         assert(hits >= MinimumHits, "expected hit ratio");
+        AddressPool.ReleaseListeningAddress(address);
+    }
+
+    async testCachedUrlPurged() {
+        const address = AddressPool.ReserveListeningAddress();
+        assert(this.cachedUrl);
+        let resource = new Resource();
+        resource.uri.address = address;
+        resource.uri = this.cachedUrl;
+        resource.makeCachable();
+        resource.body = new Body('x'.repeat(ResponseBodyBytes));
+        resource.finalize();
+
+        await this.doMiss(resource, 1, "test that the initially cached URL was purged");
         AddressPool.ReleaseListeningAddress(address);
     }
 
@@ -105,6 +138,7 @@ export default class MyTest extends Test {
         await this.testStep(1, "Fill the cache");
         await this.testStep(2, "Overfill the cache");
         await this.testStep(3, "Check");
+        await this.testCachedUrlPurged();
         AddressPool.ReleaseListeningAddress(address);
     }
 
