c77c10fbe1c17098458e583db50df7284c92c63b
Eduard Bagdasaryan <dictiano@gmail.com>
2023-07-20 02:24:07 +0300

    Adjusted with recent Daft features
    such as 'pokeSameWorker' parameter and others. Works with Daft ee890db.
    Also documented the 'finishCaching' problem for updates.

--- tests/accumulate-headers-after-304.js
+++ tests/accumulate-headers-after-304.js
@@ -23,10 +23,14 @@ const DefaultPrefixSize = DefaultPrefixSizeKB*1024;
 
 Config.Recognize([
     {
-        option: "smp",
+        option: "workers",
+        type: "Number",
+        description: "the number of Squid worker processes",
+    },
+    {
+        option: "poke-same-worker",
         type: "Boolean",
-        default: "false",
-        description: "In this mode MISS, UPDATE and HIT requests will go to different proxy SMP workers",
+        description: "send all test case requests to the same Squid worker process",
     },
     {
         option: "max-prefix-size",
@@ -34,42 +38,41 @@ Config.Recognize([
         default: DefaultPrefixSize.toString(),
         description: "maximum message body size (KB)",
     },
-
 ]);
 
 export default class MyTest extends Test {
 
     _configureDut(cfg) {
-        assert(!Config.dutDiskCache() || Config.smp());
         assert(Config.dutMemoryCache() || Config.dutDiskCache());
-        if (Config.smp()) {
-            cfg.workers(4);
-            cfg.dedicatedWorkerPorts(true);
-            this._workerListeningAddresses = cfg.workerListeningAddresses();
-        } 
-        cfg.custom(`reply_header_max_size ${Config.MaxPrefixSize} KB`);
+        cfg.workers(Config.workers());
+        cfg.dedicatedWorkerPorts(Config.workers() > 1);
+        this._workerListeningAddresses = cfg.workerListeningAddresses();
+        cfg.custom(`reply_header_max_size ${Config.maxPrefixSize()} KB`);
     }
 
     static Configurators() {
         const configGen = new ConfigurationGenerator.FlexibleConfigGen();
 
-        configGen.smp(function *(cfg) {
+        configGen.workers(function *() {
+            yield 1;
+            yield 5;
+        });
+
+        configGen.pokeSameWorker(function *(cfg) {
+            if (cfg.workers() > 1) // poking different workers requires multiple workers
+                yield false;
+            yield true;
+        });
+
+        configGen.dutMemoryCache(function *(cfg) {
             yield false;
             yield true;
         });
 
         configGen.dutDiskCache(function *(cfg) {
-            yield false;
-            if (cfg.smp()) {
-                yield true;
-            }
-        });
-
-        configGen.dutMemoryCache(function *(cfg) {
-            yield true;
-            if (cfg.dutDiskCache()) {
+            if (cfg.dutMemoryCache()) // do not end up with no caching at all
                 yield false;
-            }
+            yield true;
         });
 
         configGen.maxPrefixSize(function *(cfg) {
@@ -79,11 +82,11 @@ export default class MyTest extends Test {
             yield 128;
             yield 2048;
         });
-        
+
         return configGen.generateConfigurators();
     }
 
-    maxPrefixSize() { return Config.MaxPrefixSize * 1024; }
+    maxPrefixSize() { return Config.maxPrefixSize() * 1024; }
 
     async run(/*testRun*/) {
         let resource = new Resource();
@@ -101,13 +104,12 @@ export default class MyTest extends Test {
         let updateField = new Field(updateHeaderName, 'x');
         updateField.finalize();
         const updateHeaderLength = updateField.raw().length;
-        
+
         {
             const prefixSize = this.maxPrefixSize() - updateHeaderLength;
             let testCase = new HttpTestCase(`forward a cachable response with a prefix size less than the maximum allowed ${prefixSize}<${this.maxPrefixSize()}`);
             testCase.client().request.for(resource);
-            if (Config.smp())
-                testCase.client().nextHopAddress = this._workerListeningAddresses[1];
+            testCase.client().nextHopAddress = this._workerListeningAddressFor(1);
             testCase.server().serve(resource);
             testCase.server().response.enforceMinimumPrefixSize(prefixSize);
             testCase.server().response.header.add(hitCheck);
@@ -120,13 +122,13 @@ export default class MyTest extends Test {
         {
             let testCase = new HttpTestCase('respond with a 304 hit');
             testCase.client().request.for(resource);
-            if (Config.smp())
-                testCase.client().nextHopAddress = this._workerListeningAddresses[1];
+            testCase.client().nextHopAddress = this._workerListeningAddressFor(2);
             testCase.client().request.conditions({ ims: resource.notModifiedSince() });
             testCase.client().checks.add((client) => {
                 client.expectStatusCode(304);
             });
             await testCase.run();
+            await this.dut.finishCaching();
         }
 
         let updatingResponse = null;
@@ -139,8 +141,7 @@ export default class MyTest extends Test {
             testCase.client().request.for(resource);
             testCase.client().request.conditions({ ims: resource.modifiedSince() });
             testCase.client().request.header.add("Cache-Control", "max-age=0");
-            if (Config.smp())
-                testCase.client().nextHopAddress = this._workerListeningAddresses[2];
+            testCase.client().nextHopAddress = this._workerListeningAddressFor(3);
 
             testCase.server().response.header.add(updateField);
             testCase.server().response.startLine.code(304);
@@ -151,19 +152,18 @@ export default class MyTest extends Test {
                 assert(receivedResponse.startLine.codeInteger() === 200);
                 assert.equal(receivedResponse.header.value(hitCheck.name), hitCheck.value, "preserved originally cached header field");
             });
-            await testCase.run();
 
-            // need some time to update the cached entry for disk-only SMP configurations
-            // without it the initially cached entry may be returned
-            // TODO: a better way to achieve this?
-            await Gadgets.SleepMs(1000);
+            await testCase.run();
+            // XXX: Does not work when updating headers
+            // await this.dut.finishCaching();
         }
 
         {
             let testCase = new HttpTestCase(`hit the cached entry with the maximum allowed prefix: ${this.maxPrefixSize()} `);
             testCase.client().request.for(resource);
-            if (Config.smp())
-                testCase.client().nextHopAddress = this._workerListeningAddresses[3];
+            // TODO: should be step '4'. Temporarily set as the previous step to overcome
+            // the 'dut.finishCaching()' problem for updates
+            testCase.client().nextHopAddress = this._workerListeningAddressFor(3);
             testCase.check(() => {
                 let updatedResponse = testCase.client().transaction().response;
                 const code = updatedResponse.startLine.codeInteger();
@@ -183,8 +183,7 @@ export default class MyTest extends Test {
             testCase.client().request.for(resource);
             testCase.client().request.conditions({ ims: resource.modifiedSince() });
             testCase.client().request.header.add("Cache-Control", "max-age=0");
-            if (Config.smp())
-                testCase.client().nextHopAddress = this._workerListeningAddresses[2];
+            testCase.client().nextHopAddress = this._workerListeningAddressFor(5);
 
             testCase.server().response.header.addOverwrite(updateHeaderName, "xy");
 
@@ -207,6 +206,31 @@ export default class MyTest extends Test {
         }
 
         AddressPool.ReleaseListeningAddress(resource.uri.address);
+    }
+
+    // TODO: Move/Refactor into Proxy::workerForStep().primaryAddress(): The
+    // primary listening address of the round-robin selected worker. Here,
+    // "primary" means a worker-designated address (if it exists) or the
+    // general proxy listening address (otherwise).
+    _workerListeningAddressFor(stepId)
+    {
+        assert(stepId >= 1);
+
+        let workerId = 1;
+        if (!Config.pokeSameWorker()) {
+            // use workers in round-robin fashion, using the first worker for
+            // the first step; both worker and step IDs are 1-based
+            assert(Config.workers() > 0);
+            workerId = 1 + ((stepId-1) % Config.workers());
+        }
+
+        // The first this._workerListeningAddresses element is a well-known
+        // port address shared by all workers. We do not use it here.
+        const offset = 1 + (workerId - 1);
+        assert(offset >= 0);
+        assert(offset < this._workerListeningAddresses.length);
+        console.log("Adress: === " + this._workerListeningAddresses[offset].port + " " + offset + " " + this._workerListeningAddresses.length);
+        return this._workerListeningAddresses[offset];
     }
 }
 
