d407a3e08b29c1c868dbde98403356d5feb1e5ff
Alex Rousskov <rousskov@measurement-factory.com>
2025-04-08 15:35:51 -0400

    tests/smooth-reconfiguration-of-cache-peers: cache_peer_access
    Also removed duplicated 'GET' parameters from _makeTestCase() calls.
    
    Requires Daft commit d7314fda that added
    CachePeer::Config::becomeAttractedToAll() and becomeAttractedToNone()
    methods.

--- tests/smooth-reconfiguration-of-cache-peers.js
+++ tests/smooth-reconfiguration-of-cache-peers.js
@@ -2,7 +2,7 @@
 // Copyright (C) The Measurement Factory.
 // http://www.measurement-factory.com/
 
-// Checks whether smooth reconfiguration of cache_peers works.
+// Test smooth reconfiguration of cache_peer and cache_peer_access directives.
 
 import assert from "assert";
 
@@ -43,9 +43,13 @@ export default class MyTest extends Test {
     }
 
     // HttpTestCase configuration shared among all test cases
-    _makeTestCase(requestMethod, goal, cachePeer) {
-        const testCase = new HttpTestCase(`${goal}; ${requestMethod} to cache_peer name=${cachePeer.config().name()}`);
+    _makeTestCase(goal, cachePeerToTarget, cachePeerThatShouldReceiveRequest = null) {
+        if (!cachePeerThatShouldReceiveRequest)
+            cachePeerThatShouldReceiveRequest = cachePeerToTarget;
 
+        const testCase = new HttpTestCase(goal);
+
+        const requestMethod = 'GET';
         testCase.client().request.startLine.method = requestMethod;
         testCase.client().request.startLine.uri.address = {
             host: Config.originAuthority().host,
@@ -56,7 +60,7 @@ export default class MyTest extends Test {
         // we do not need a server because all traffic goes through
         // cache_peers that generate (i.e. do not forward) responses
 
-        cachePeer.config().attract(testCase.client().request);
+        cachePeerToTarget.config().attract(testCase.client().request);
 
         testCase.check(async () => {
             const accessRecords = await this.dut.getNewAccessRecords();
@@ -66,7 +70,7 @@ export default class MyTest extends Test {
             this._lastAccessRecord = accessRecord;
         });
 
-        if (cachePeer.config().hidden()) {
+        if (cachePeerToTarget.config().hidden()) {
             testCase.check(async () => {
                 testCase.expectStatusCode(502);
 
@@ -80,7 +84,7 @@ export default class MyTest extends Test {
         } else {
             testCase.check(async () => {
                 testCase.expectStatusCode(200);
-                testCase.client().transaction().response.header.expectFieldValueAmongOthers(cachePeer.response.header.has("Via"));
+                testCase.client().transaction().response.header.expectFieldValueAmongOthers(cachePeerThatShouldReceiveRequest.response.header.has("Via"));
 
                 this._lastAccessRecord.checkUnknown('%err_code');
                 this._lastAccessRecord.checkUnknown('%err_detail');
@@ -101,32 +105,39 @@ export default class MyTest extends Test {
         const cachePeerA = this.dut.cachePeers()[0];
         const cachePeerB = this.dut.cachePeers()[1];
 
-        await this._makeTestCase('GET', 'peerA baseline', cachePeerA).run();
-        await this._makeTestCase('GET', 'peerB baseline', cachePeerB).run();
+        await this._makeTestCase('peerA baseline', cachePeerA).run();
+        await this._makeTestCase('peerB baseline', cachePeerB).run();
 
         await this.dut.reconfigureWithoutChanges(true);
-        await this._makeTestCase('GET', 'peerA routing after no-changes reconfiguration', cachePeerA).run();
-        await this._makeTestCase('GET', 'peerB routing after no-changes reconfiguration', cachePeerB).run();
+        await this._makeTestCase('peerA routing after no-changes reconfiguration', cachePeerA).run();
+        await this._makeTestCase('peerB routing after no-changes reconfiguration', cachePeerB).run();
 
         // TODO: Resume honoring these problems (as needed) later.
         this.dut.ignoreProblems(/WARNING: Removing old cache_peer.*\bpeerA\b/);
         cachePeerA.config().hide("testing removal of the first cache_peer");
         await this.dut.reconfigureAfterChanges();
-        await this._makeTestCase('GET', 'peerA routing after peerA removal', cachePeerA).run();
-        await this._makeTestCase('GET', 'peerB routing after peerA removal', cachePeerB).run();
+        await this._makeTestCase('peerA routing after peerA removal', cachePeerA).run();
+        await this._makeTestCase('peerB routing after peerA removal', cachePeerB).run();
 
         this.dut.ignoreProblems(/WARNING: Removing old cache_peer.*\bpeerB\b/);
         cachePeerB.config().hide("testing removal of the last remaining cache_peer");
         await this.dut.reconfigureAfterChanges();
-        await this._makeTestCase('GET', 'peerA routing after peerB removal', cachePeerA).run();
-        await this._makeTestCase('GET', 'peerB routing after peerB removal', cachePeerB).run();
+        await this._makeTestCase('peerA routing after peerB removal', cachePeerA).run();
+        await this._makeTestCase('peerB routing after peerB removal', cachePeerB).run();
 
         cachePeerB.config().show();
         await this.dut.reconfigureAfterChanges();
-        // TODO: Enable the case below after making cache_peer_access pliable:
-        // Unchanged rigid cache_peer_access directives are ignored for added
-        // cache_peers (e.g., peerB in this case that was re-added above).
-        // await this._makeTestCase('GET', 'peerA routing after peerB resurrection', cachePeerA).run();
-        await this._makeTestCase('GET', 'peerB routing after peerB resurrection', cachePeerB).run();
+        await this._makeTestCase('peerA routing after peerB resurrection', cachePeerA).run();
+        await this._makeTestCase('peerB routing after peerB resurrection', cachePeerB).run();
+
+        cachePeerA.config().show();
+        await this.dut.reconfigureAfterChanges();
+        await this._makeTestCase('peerA routing after peerA resurrection', cachePeerA).run();
+        await this._makeTestCase('peerB routing after peerA resurrection', cachePeerB).run();
+
+        cachePeerA.config().becomeAttractedToNone(); // only modifies cache_peer_access directives
+        cachePeerB.config().becomeAttractedToAll(); // only modifies cache_peer_access directives
+        await this.dut.reconfigureAfterChanges();
+        await this._makeTestCase('re-routing from peerA to peerB via cache_peer_access mods', cachePeerA, cachePeerB).run();
     }
 }
