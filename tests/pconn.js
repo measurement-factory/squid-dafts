7c693d928f77ad0c8c2d413805f8c0204935d4e5
Alex Rousskov <rousskov@measurement-factory.com>
2026-03-06 17:44:04 -0500

    tests/pconn: Check persistent client-Squid connection reuse
    Requires Daft master commit 0c903a95.

--- tests/pconn.js
+++ tests/pconn.js
@@ -2,7 +2,7 @@
 // Copyright (C) The Measurement Factory.
 // http://www.measurement-factory.com/
 
-// Check whether the proxy reuses a persistent connection with the server.
+// Check whether the proxy reuses persistent connections with the client and server.
 
 import assert from "assert";
 import HttpTestCase from "../src/test/HttpCase";
@@ -17,25 +17,49 @@ export default class MyTest extends Test {
         resource.uri.address = AddressPool.ReserveListeningAddress();
         resource.finalize();
 
-        // TODO: Support client-proxy pconns as well.
-
-        const openCase = new HttpTestCase('create a proxy-server pconn');
+        const openCase = new HttpTestCase('create pconns');
         openCase.client().request.for(resource);
         openCase.server().serve(resource);
+        openCase.client().keepConnections();
         openCase.server().keepConnections();
+        openCase.expectAccessRecordChecks(this.dut);
         openCase.addMissCheck();
+        openCase.client().checks.add((client) => {
+            assert(client.transaction().request.persistent()); // code check
+            assert(client.transaction().response.persistent());
+        });
         openCase.server().checks.add((server) => {
-            assert(server.transaction().response.persistent());
+            assert(server.transaction().request.persistent());
+            assert(server.transaction().response.persistent()); // code check
+        });
+        openCase.check(() => {
+            openCase.accessRecords().single().checkKnown('%transport::>connection_id');
         });
         await openCase.run();
 
-        const reuseCase = new HttpTestCase('reuse a proxy-server pconn');
+        const reuseCase = new HttpTestCase('reuse pconns');
         reuseCase.client().request.for(resource);
         reuseCase.server().serve(resource);
+        reuseCase.client().reuseConnectionsFrom(openCase.client());
         reuseCase.server().reuseConnectionsFrom(openCase.server());
+        reuseCase.expectAccessRecordChecks(this.dut);
         reuseCase.addMissCheck();
+        reuseCase.client().checks.add((client) => {
+            // Without keepConnections(), the client should signal connection
+            // closure, and the proxy has to respond in kind.
+            assert(!client.transaction().request.persistent()); // code check
+            assert(!client.transaction().response.persistent());
+            assert.strictEqual(client.transaction().reusedTransportConnection(), true);
+        });
         reuseCase.server().checks.add((server) => {
+            // The proxy may signal connection reuse, but the server must
+            // signal connection closure since there was no keepConnections().
+            assert(!server.transaction().response.persistent()); // code check
             assert.strictEqual(server.transaction().reusedTransportConnection(), true);
+        });
+        reuseCase.check(() => {
+            // TODO: Check %transport::<connection_id when Squid supports that.
+            reuseCase.accessRecords().single().checkEqualIn('%transport::>connection_id', openCase.accessRecords().single());
         });
         await reuseCase.run();
     }
